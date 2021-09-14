40faf407a067fcf9e7a3e2ebb44e7fc8760c28a4
sudaltokki <mary000605@naver.com>
2021-09-15 08:17:57 +0900

    made script.js

---
+++ 박지원/script.js
@@ -0,0 +1,6 @@
+function local() {
+    var user_name = document.getElementById("name").value;
+    var user_id = document.getElementById("stdid").value;
+    
+    alert("이름: " + user_name + ", 학번: " + user_id);
+}
