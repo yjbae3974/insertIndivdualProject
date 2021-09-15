67ee58dad6051d11d304d42018b0950d20a1f9d6
sudaltokki <mary000605@naver.com>
2021-09-15 13:05:37 +0900

    add myObj.js

--- 박지원/myObj.js
+++ 박지원/myObj.js
@@ -1,6 +1,29 @@
-function local() {
-    var user_name = document.getElementById("name").value;
-    var user_id = document.getElementById("stdid").value;
-    
-    alert("이름: " + user_name + ", 학번: " + user_id);
+function User(name, id) {
+
+}
+var user = new User();
+
+function f() {
+   alert(user.name);
+}
+
+
+window.onload = function(){
+   document.getElementById('info-name').innerHTML = user.name;
+}
+
+function user_login() {
+    user.name = document.getElementById("name").value;
+    user.id = document.getElementById("stdid").value;
+
+    if (user.name && user.id) {
+        alert(user.id + " " + user.name + "님 환영합니다.");
+        location.assign("./page1.html");
+    } else if (user.name && !(user.id)) {
+        alert("학번을 입력해주세요.");
+    } else if (user.id && !(user.name)) {
+        alert("이름을 입력해주세요.");
+    } else {
+        alert("이름과 학번을 입력해주세요.");
+    }
 }
