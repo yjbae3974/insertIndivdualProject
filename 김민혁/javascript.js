7aaf98820f48370e2db93dc3d3f9c85a0fcf36fb
alsgur7859 <alsgur7859@naver.com>
2021-09-21 14:55:41 +0900

    almost? final(except localstorage)

--- 김민혁/javascript.js
+++ 김민혁/javascript.js
@@ -1,17 +1,11 @@
-
-// function alignList(list){
-    // li = list.querySelector('li');
-    // $(list).css({
-        // 'border-left': 'solid black 1px',
-        // 'border-right': 'solid black 1px'
-    // });
-// }
 let vocas = new Array();
-// let lis = new Array();
+let hello = 1;
+const ul = document. getElementsByClassName("vocabularySets")[0];
+
 
 function addVocaSet(voca, meaning) {
     var vocaSet = new Object();
-    var ul = document. getElementsByClassName("vocabularySets")[0];
+    // var ul = document. getElementsByClassName("vocabularySets")[0];
     var li = document. createElement("li");
     vocaSet.voca = voca;
     vocaSet.meaning = meaning;
@@ -25,51 +19,21 @@ function addVocaSet(voca, meaning) {
 }
 
 function modifyVocaSet(voca, meaning){
-    var ul = document. getElementsByClassName("vocabularySets")[0];
+    // var ul = document. getElementsByClassName("vocabularySets")[0];
     var liIndex =  vocas.findIndex(obj => {return obj.voca === voca});
     // console.log(a);
     // console.log($("ul li:nth-child("+a+")").firstChild);
     ul.children[liIndex].childNodes[0].nodeValue = voca + "\t" + meaning;
     console.log(ul.children[liIndex].childNodes[0]);
-    //  = voca + "\t" + meaning;
-    // innerText = voca + "\t" + meaning;
 }
-//vocas.findIndex(obj => {return obj.voca === voca})
-
-// function addVocaSet(voca, meaning) {
-//     var vocaSet = new Object();
-//     var ul = document. getElementsByClassName("vocabularySets")[0];
-//     var li = document. createElement("li");
-//     vocaSet.voca = voca;
-//     vocaSet.meaning = meaning;
-//     vocas.push(vocaSet);
-//     li. appendChild(document. createTextNode(voca + '\t' + meaning));
-//     ul. appendChild(li);
-//     var button = document.createElement("button");
-//     li.appendChild(button);
-//     button.innerText = "X";
-//     button.addEventListener("click", removeButton);
-// }
 
 
-// function showVocaSet(){
-//     for (i = 0; i < array.length; i++)
-//     document.writeln((i+1) + ": " + array[i]);
-//     var ul = document. getElementsByClassName("vocabularySets")[0];
-//     var li = document. createElement("li");
-//     for
-//     li. appendChild(document. createTextNode(voca + '\t' + meaning));
-//     ul. appendChild(li);
-//     var button = document.createElement("button");
-//     li.appendChild(button);
-//     button.innerText = "X";
-//     button.addEventListener("click", removeButton);//remove버튼에 array에서도 지우는 기능 추가
-// }
 
 
 function plusClicked(){
     var addVocabulary = document.getElementsByClassName('vocabulary')[0].value.trim();
     var addMeaning = document.getElementsByClassName('meaning')[0].value.trim();
+    hello = 2;
 
     if (!addVocabulary | !addMeaning){
         alert('fill the blank!');
@@ -78,34 +42,45 @@ function plusClicked(){
         if(vocas.find(obj => {return obj.voca === addVocabulary})){
 
             var splitMeaning = vocas[vocas.findIndex(obj => {return obj.voca === addVocabulary})].meaning.split(/[\s,]+/);
+            var splitAddMeaning = addMeaning.split(/[\s,]+/);
+            var sameVocaMeaning = [];
             var accord = false;
             for(var i = 0; i < splitMeaning.length; i++){
-                if(splitMeaning[i] === addMeaning){
-                    accord = true;
-                    break;
+                for(var j = 0; j < splitAddMeaning.length; j++){
+                    if(splitMeaning[i] === splitAddMeaning[j]){
+                        sameVocaMeaning.push(splitAddMeaning[j]);
+                        accord = true;
+                        break;
+                    }
                 }
             }
 
             if(accord){
-                alert("already have same voca!");
-
+                var str = "already have same voca! (" + addVocabulary + ": ";
+                for(var i = 0; i < sameVocaMeaning.length; i++){
+                    str += (sameVocaMeaning[i]);
+                    if(i === sameVocaMeaning.length - 1){
+                        continue;
+                    }
+                    str += ", ";
+                }
+                str += ")";
+                alert(str);
             }
             else{
                 vocas[vocas.findIndex(obj => {return obj.voca === addVocabulary})].meaning = vocas[vocas.findIndex(obj => {return obj.voca === addVocabulary})].meaning + ", " + addMeaning;
                 modifyVocaSet(vocas[vocas.findIndex(obj => {return obj.voca === addVocabulary})].voca, vocas[vocas.findIndex(obj => {return obj.voca === addVocabulary})].meaning);
-                // showVocaSet();
             }
         }
         else{
             addVocaSet(addVocabulary, addMeaning);
-            // showVocaSet();
         }
     }
 }
 
     
-function removeButton(event){//remove버튼에 array에서도 지우는 기능 추가
-    console.log(vocas.findIndex(obj => {return obj.voca === event.target.parentElement.innerText.split(' ')[0]}));
+function removeButton(event){
+    // console.log(vocas.findIndex(obj => {return obj.voca === event.target.parentElement.innerText.split(' ')[0]}));
     vocas.splice(vocas.findIndex(obj => {return obj.voca === (event.target.parentElement.innerText.split(' ')[0])}), 1);
     var removingOne = event.target.parentElement;
     removingOne.remove();
@@ -113,8 +88,34 @@ function removeButton(event){//remove버튼에 array에서도 지우는 기능 �
 
 function reset(){
     vocas = [];
-    document.getElementsByClassName('vocabularySets')[0].innerHTML = '';
+    ul.innerHTML = '';
+    //reset할 때 텍스트 입력창의 글자도 지워지면 좋겠는데 방법을 모르겠네
 }
+
+    // console.log(document. getElementsByClassName("vocabularySets")[0].innerHTML)
+
+
+// for (var i = 0; i < vocas.length; i++){
+//     localStorage.setItem(vocas.voca, vocas.meaning);
+// }
+
+// console.log(localStorage.length);
+
+// for (var i = 0; i < localStorage.length; i++){
+//     const key = localStorage.key(i);
+//     const value = localStorage.getItem(key);
+//     document. getElementsByClassName("vocabularySets")[0].innerHTML += "<li>"+key+"\t"+value+"</li>";
+//     console.log(localStorage);
+// }
+
+// console.log(localStorage.length);
+
+// for(var i = 0; i < 5; i++){
+//     var uli = document. getElementsByClassName("vocabularySets")[0];
+//     var li = document. createElement("li");
+//     li. appendChild(document. createTextNode(i));
+//     console.log(uli. innerHTML);
+// }
 
 // function createRemoveButton(){
 //     var remove_btn = documnet.createElement("BUTTON");
